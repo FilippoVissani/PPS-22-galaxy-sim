5eb715936698db6628d09db8cc6c31e15c2fe70a
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-09-16 11:57:03 +0200

    implemented basic circular collision detection

--- src/main/scala/physics/collisions/Collisions.scala
+++ src/main/scala/physics/collisions/Collisions.scala
@@ -1,3 +1,12 @@
 package physics.collisions
 
-object Collisions
+object Collisions:
+  case class P2d(x: Float, y: Float)
+  sealed trait Collider
+  case class CircleCollider(origin: P2d, radius: Float)
+  class CircleToCircleDetector:
+    def detect(c1: CircleCollider, c2: CircleCollider): Boolean =
+      val dx = c1.origin.x - c2.origin.x
+      val dy = c1.origin.y - c2.origin.y
+      val dist = Math.sqrt(dx*dx + dy*dy)
+      dist < c1.radius + c2.radius
