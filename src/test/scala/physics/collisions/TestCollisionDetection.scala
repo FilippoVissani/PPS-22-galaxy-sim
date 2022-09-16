5d4b3b711057ce29df3201d480d8b430c1ca7a95
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-09-16 12:00:12 +0200

    collision test

---
+++ src/test/scala/physics/collisions/TestCollisionDetection.scala
@@ -0,0 +1,24 @@
+package physics.collisions
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import physics.collisions.Collisions.P2d
+import physics.collisions.Collisions.CircleToCircleDetector
+
+
+class TestCollisionDetection extends AnyFlatSpec:
+  import Collisions.CircleCollider
+
+  "A CollisionDetector" should "detect collision between two colliders" in {
+    val c1 = CircleCollider(P2d(0,0), 2)
+    val c2 = CircleCollider(P2d(1,1), 0.5)
+    val detector = new CircleToCircleDetector
+    assert(detector.detect(c1, c2))
+  }
+
+  it should "not detect collisions between colliders that are too distant" in {
+    val c1 = CircleCollider(P2d(0,0), 0.1)
+    val c2 = CircleCollider(P2d(1,1), 0.5)
+    val detector = new CircleToCircleDetector
+    assert(!detector.detect(c1, c2))
+  }
