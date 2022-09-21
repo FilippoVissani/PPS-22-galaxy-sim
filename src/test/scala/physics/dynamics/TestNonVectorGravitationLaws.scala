2ddbe18aeb4a41be7048e7821a3f0a053cb4fcd7
Angela Cortecchia <angela.cortecchia@hotmail.com>
2022-09-21 17:31:24 +0200

    refactor test

--- src/test/scala/physics/dynamics/TestNonVectorGravitationLaws.scala
+++ src/test/scala/physics/dynamics/TestNonVectorGravitationLaws.scala
@@ -3,14 +3,14 @@ package physics.dynamics
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.funsuite.AnyFunSuite
 
-import math.{pow, tanh}
+import math.{pow, sqrt, tanh}
 
 class TestNonVectorGravitationLaws extends AnyFunSuite with BeforeAndAfterAll:
   import GravitationLaws.*
 
   val earthPos: Position = Position(106, 106)
   val sunPos: Position = Position(0, 0)
-  val time: Double = 1 * daySec
+  val time: Double = 1 * daySec //one day
   val earthSpeed: Double = 29290
   val earthMass: Double = 5.972e24 //earth
   val sunMass: Double = 2.0e30 //sun
@@ -26,40 +26,35 @@ class TestNonVectorGravitationLaws extends AnyFunSuite with BeforeAndAfterAll:
   var changeDisplacement: Double = 0.0
 
   test("Distance between two points"){
-    dist = distanceBetweenTwoEntities(earth.position, sun.position)
-    val dist2 = distanceBetweenTwoEntities(earthPos, sunPos)
+    dist = distanceBetweenTwoEntities(earth, sun)
+    val dist2 = sqrt(pow(earthPos.x - sunPos.x, 2) + pow(earthPos.y - sunPos.y, 2))
     println("sun-earth distance " + dist)
-    assert(dist == dist2)
+    assert(dist === dist2)
   }
 
   test("Accelleration between two entities"){
     acceleration = accelerationBetweenTwoEntities(earth, sun)
     val shouldAcc = GravitationLaws.gravityCostant * (sun.mass / pow(dist, 2))
     println("sun-earth accelleration " + acceleration)
-    assert(acceleration == shouldAcc)
+    assert(acceleration === shouldAcc)
   }
 
   test("force between two entities"){
     force = forceBetweenTwoEntities(earth, sun)
     val shouldForce = acceleration * earth.mass
     println("sun-earth force " + force)
-    assert(force == shouldForce)
+    assert(force === shouldForce)
   }
 
   test("calculate speed of the entity after delta time"){
     val shouldSpeed = acceleration * time
     speed = speedAfterDeltaTime(acceleration, time)
     println(s"speed of earth after $time seconds is $speed")
-    assert(speed == shouldSpeed)
+    assert(speed === shouldSpeed)
   }
 
   test("calculate change of displacement of entity"){
     changeDisplacement = changeOfDisplacement(earth.aphelionSpeed, speed, time)
     val shouldPos = (earth.aphelionSpeed + speed) * time
-    assert(changeDisplacement == shouldPos)
-  }
-
-  test("calculate entity's new position"){
-    entityNewPosition(changeDisplacement, earth)
-    assert(false)
+    assert(changeDisplacement === shouldPos)
   }
