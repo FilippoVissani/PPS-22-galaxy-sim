68b8a4d28f4b7aa8c0af3a1ddc1060d9730d6c84
Eddie Barzi <eddie.barzi@studio.unibo.it>
2022-09-23 10:25:06 +0200

    prolog engine and method to get entity type (based on mass and temperature)

---
+++ src/main/scala/galaxy_sim/prolog/EntityIdentifierProlog.scala
@@ -0,0 +1,26 @@
+package galaxy_sim.prolog
+
+import alice.tuprolog.Theory
+import galaxy_sim.model.EntityType
+import galaxy_sim.prolog.Scala2P.{*, given}
+
+type Mass = Double
+type Temperature = Double
+
+trait EntityIdentifierProlog:
+  /**
+   * 
+   * @param mass the mass of the entity
+   * @param temperature the temperature of the entity
+   * @return the entity type as EntityType
+   */
+  def checkEntityType(mass: Mass, temperature: Temperature): EntityType
+
+object EntityIdentifierProlog:
+  def apply(): EntityIdentifierProlog = EntityIdentifierPrologImpl()
+  private case class EntityIdentifierPrologImpl() extends EntityIdentifierProlog:
+    private val engine = mkPrologEngine(Theory.parseLazilyWithStandardOperators(getClass.getResourceAsStream("/prolog/EntityIdentifier.pl")))
+    
+    override def checkEntityType(mass: Mass, temperature: Temperature): EntityType =
+      val goal = s"typeOfEntity($temperature, $mass, E)"
+      solveOneAndGetTerm(engine, goal, "E")
