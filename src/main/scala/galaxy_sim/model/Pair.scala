583d8273febb2aa311941ea49a8f5170c04fd5d8
Filippo Vissani <filippo.vissani@studio.unibo.it>
2022-09-29 11:58:38 +0200

    Added sum of Pair[Double, Double]

--- src/main/scala/galaxy_sim/model/Pair.scala
+++ src/main/scala/galaxy_sim/model/Pair.scala
@@ -1,13 +1,20 @@
 package galaxy_sim.model
 
-trait Pair[+A, +B]:
+trait Pair[A, B]:
   def x: A
   def y: B
-  def map[C, D](f: A => C )(g: B => D ): Pair[C, D]
 
 object Pair:
   def apply[A, B](x: A, y: B): Pair[A, B] = PairImpl[A, B](x, y)
+  def map[A, B, C, D](p: Pair[A, B])(f: A => C)(g: B => D): Pair[C, D] = Pair(f(p.x), g(p.y))
+  extension[A,B] (p: Pair[A,B])
+    def +(other: Pair[A, B])(using op: PairOperations[A,B]) = op.sum(p, other)
+    
+  private case class PairImpl[A, B](x: A, y: B) extends Pair[A, B]
 
-  private case class PairImpl[+A, +B](x: A, y: B) extends Pair[A, B]:
-
-    override def map[C, D](f: A => C)(g: B => D): Pair[C, D] = Pair(f(x), g(y))
+  sealed trait PairOperations[A, B]:
+    def sum(p1: Pair[A, B], p2: Pair[A, B]): Pair[A, B]
+    
+  object PairOperations:
+    given SumPairOfDouble: PairOperations[Double, Double] with
+      override def sum(p1: Pair[Double, Double], p2: Pair[Double, Double]): Pair[Double, Double] = Pair(p1.x + p2.x, p1.y + p2.y)
