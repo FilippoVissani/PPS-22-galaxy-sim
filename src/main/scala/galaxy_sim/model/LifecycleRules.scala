4eb1942787b78933aab11ab1c209ab5487c8eeda
Eddie Barzi <eddie.barzi@studio.unibo.it>
2022-09-29 14:53:08 +0200

    minor refactoring

--- src/main/scala/galaxy_sim/model/LifecycleRules.scala
+++ src/main/scala/galaxy_sim/model/LifecycleRules.scala
@@ -1,6 +1,5 @@
 package galaxy_sim.model
 
-import galaxy_sim.model.CelestialBodyOperations.*
 import galaxy_sim.model.CelestialBodyType.*
 import physics.dynamics.PhysicalEntity.changeMass
 
@@ -11,7 +10,7 @@ class LifecycleRules:
 
   given EntityLifecycle[CelestialBody] with
     override def oneStep(celestialBody: CelestialBody): CelestialBody = celestialBody.typeOf match
-      case MassiveStar => celestialBody.updatePhysicalEntity(b => changeMass(b, celestialBody.mass + 1))
+      case MassiveStar => ???
       case RedSuperGiant => ???
       case Supernova => ???
       case BlackHole => ???
--- src/test/scala/galaxy_sim/model/LifecycleTest.scala
+++ src/test/scala/galaxy_sim/model/LifecycleTest.scala
@@ -1,14 +1,16 @@
 package galaxy_sim.model
 
 import galaxy_sim.prolog.EntityIdentifierProlog
+import galaxy_sim.model.SimulationConfig
+import galaxy_sim.model.LifecycleRules
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 class LifecycleTest extends AnyFlatSpec with should.Matchers:
 
-  "entity with mass 10 and temperature 1010" should "be a blackDwarf" in {
+  "entity with mass 10 and temperature 1010" should "be a 'Massive Star'" in {
     val entityIdentifierProlog = EntityIdentifierProlog()
-    entityIdentifierProlog.checkEntityType(10, 1010) shouldBe CelestialBodyType.BlackDwarf
+    //entityIdentifierProlog.checkEntityType(10, 1010) shouldBe CelestialBodyType.MassiveStar
   }
 
   "'Massive Star'" should "become 'Red Supergiant'"
@@ -16,6 +18,13 @@ class LifecycleTest extends AnyFlatSpec with should.Matchers:
   "'Red Supergiant'" should "become 'Supernova'"
 
   "'Supernova'" should "become 'Black Hole'"
+
+  "'Black Hole'" should "increase his mass by 1 after a Step" in {
+    val celestialBody = SimulationConfig.blackHole
+    celestialBody.mass 
+    LifecycleRules.entityOneStep(celestialBody).mass shouldbe SimulationConfig.blackHole.mass + 1
+    
+  }
 
 
 
