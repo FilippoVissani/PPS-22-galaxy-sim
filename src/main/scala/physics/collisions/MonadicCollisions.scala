0e5f51be4204fd620027175d50bac22de71fac60
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-10-01 09:27:50 +0200

    experimenting with monadic collision management

---
+++ src/main/scala/physics/collisions/MonadicCollisions.scala
@@ -0,0 +1,28 @@
+package physics.collisions
+
+import physics.Pair
+import physics.collisions.CollisionDetection.Colliders.CircleCollider
+
+object MonadicCollisions extends App:
+  // The monad Collision[A, B] encapsulates the possibility of a: A colliding with b: B
+  sealed trait Collision[A, B]:
+    def map[C](f: A => C): Collision[C, B]
+    def flatMap[C](f: A => Collision[C, B]): Collision[C, B]
+    def check(f: (A, B) => Boolean): Collision[A, B]
+
+  case class SuccessfulCollision[A, B](a: A, b: B) extends Collision[A, B]:
+    override def map[C](f: A => C): Collision[C, B] = SuccessfulCollision(f(a), b)
+    override def flatMap[C](f: A => Collision[C, B]): Collision[C, B] = f(a)
+    override def check(f: (A, B) => Boolean): Collision[A, B] =
+      if f(a, b) then SuccessfulCollision(a, b) else FailedCollision()
+
+  case class FailedCollision[A, B]() extends Collision[A, B]:
+    override def map[C](f: A => C): Collision[C, B] = FailedCollision()
+    override def flatMap[C](f: A => Collision[C, B]): Collision[C, B] = FailedCollision()
+    override def check(f: (A, B) => Boolean): Collision[A, B] = FailedCollision()
+
+  @main def main() =
+    val a = CircleCollider(Pair(0, 0), 2)
+    val b = CircleCollider(Pair(1, 1), 0.5)
+    val collider = SuccessfulCollision(a, b)
+    collider.check((_: CircleCollider, _: CircleCollider) => true).map { a => println(a) } //CircleCollider(Pair(0, 0), 2)
