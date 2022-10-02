ebaa35d05f3925f994a829e33a91b38483171eae
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-10-02 18:54:38 +0200

    refactored monadic collision to be represented by an enum

--- src/main/scala/physics/collisions/MonadicCollisions.scala
+++ src/main/scala/physics/collisions/MonadicCollisions.scala
@@ -5,34 +5,37 @@ import physics.collisions.CollisionDetection.Colliders.CircleCollider
 
 object MonadicCollisions extends App:
   // The monad Collision[A, B] encapsulates the possibility of a: A colliding with b: B
-  sealed trait Collision[A, B]:
-    def map[C](f: A => C): Collision[C, B]
-    def flatMap[C](f: A => Collision[C, B]): Collision[C, B]
-    def check(f: (A, B) => Boolean): Collision[A, B]
+  enum Collision[A, B]:
+    case Cons(a: A, b: B)
+    case None()
 
-  case class SuccessfulCollision[A, B](a: A, b: B) extends Collision[A, B]:
-    override def map[C](f: A => C): Collision[C, B] = SuccessfulCollision(f(a), b)
-    override def flatMap[C](f: A => Collision[C, B]): Collision[C, B] = f(a)
-    override def check(f: (A, B) => Boolean): Collision[A, B] =
-      if f(a, b) then SuccessfulCollision(a, b) else FailedCollision()
-
-  case class FailedCollision[A, B]() extends Collision[A, B]:
-    override def map[C](f: A => C): Collision[C, B] = FailedCollision()
-    override def flatMap[C](f: A => Collision[C, B]): Collision[C, B] = FailedCollision()
-    override def check(f: (A, B) => Boolean): Collision[A, B] = FailedCollision()
+  export Collision.*
+  extension[A, B](c: Collision[A, B])
+    def map[C](f: A => C): Collision[C, B] = c match
+      case Cons(a, b) => Cons(f(a), b)
+      case None() => None()
+    def flatMap[C](f: A => Collision[C, B]): Collision[C, B] = c match
+      case Cons(a, _) => f(a)
+      case None() => None()
 
 
   object Collision:
+    def apply[A, B](a: A, b: B): Collision[A, B] = Collision.Cons(a, b)
+
+    def check[A, B](c: Collision[A, B], f: (A, B) => Boolean): Collision[A, B] = c match
+      case Cons(a, b) => if f(a, b) then Collision(a, b) else None()
+      case None() => None()
+
     def solve[A, B, C](c: Collision[A, B])(checker: (A, B) => Boolean)(mapper: A => C): Collision[C, B] =
-      c.check(checker).map(mapper)
+      check(c, checker).map(mapper)
     def solveMany[A, B, C](l: List[Collision[A, B]])(checker: (A, B) => Boolean)(mapper: A => C): List[Collision[C, B]] =
       l.collect(c => solve(c)(checker)(mapper))
 
   @main def main(): Unit =
     val a = CircleCollider(Pair(0, 0), 2)
     val b = CircleCollider(Pair(1, 1), 0.5)
-    val collider = SuccessfulCollision(a, b)
-    val check = for
-      x <- collider.check((_: CircleCollider, _: CircleCollider) => true)
+    val collider = Collision(a, b)
+    val g = for
+      x <- check(collider, (_: CircleCollider, _: CircleCollider) => true)
     yield x
-    println(check)
+    println(g)
