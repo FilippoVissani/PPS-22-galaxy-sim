88bb354442f49cce05536f3084ef075109c9ae18
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-10-03 11:23:08 +0200

    implemented filter and refactored check function to use it

--- src/main/scala/physics/collisions/MonadicCollisions.scala
+++ src/main/scala/physics/collisions/MonadicCollisions.scala
@@ -17,16 +17,26 @@ object MonadicCollisions extends App:
     def flatMap[C](f: A => Collision[C, B]): Collision[C, B] = c match
       case Cons(a, _) => f(a)
       case None() => None()
+    def filter(p: (A, B) => Boolean): Collision[A, B] = c match
+      case Cons(a, b) => if p(a, b) then Cons(a, b) else None()
+      case None() => None()
   
   object Collision:
     def apply[A, B](a: A, b: B): Collision[A, B] = Collision.Cons(a, b)
-    def check[A, B](c: Collision[A, B], f: (A, B) => Boolean): Collision[A, B] = c match
-      case Cons(a, b) => if f(a, b) then Collision(a, b) else None()
-      case None() => None()
-    def solve[A, B, C](c: Collision[A, B])(checker: (A, B) => Boolean)(mapper: A => C): Collision[C, B] =
-      check(c, checker).map(mapper)
-    def solveMany[A, B, C](l: List[Collision[A, B]])(checker: (A, B) => Boolean)(mapper: A => C): List[Collision[C, B]] =
-      l.collect(c => solve(c)(checker)(mapper))
+    def check[A, B](c: Collision[A, B])(using f: CollisionChecker[A, B]): Collision[A, B] =
+      c.filter(f.check)
+    def solve[A, B, C](c: Collision[A, B])(using checker: CollisionChecker[A, B])(mapper: A => C): Collision[C, B] =
+      check(c).map(mapper)
+    def solveMany[A, B, C](l: List[Collision[A, B]])(using checker: CollisionChecker[A, B])(mapper: A => C): List[Collision[C, B]] =
+      l.collect(c => solve(c)(mapper))
+
+
+  trait CollisionChecker[A, B]:
+    def check(a: A, b: B): Boolean
+
+  object CollisionCheckers:
+    given CustomChecker: CollisionChecker[CircleCollider, CircleCollider] with
+      override def check(a: CircleCollider, b: CircleCollider): Boolean = true
 
   @main def main(): Unit =
     val a = CircleCollider(Pair(0, 0), 2)
