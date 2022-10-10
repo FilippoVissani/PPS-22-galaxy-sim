0d9ee87e5cdcde7c0abaf5ca3f2d9bb49fc9b91d
Eddie Barzi <eddie.barzi@studio.unibo.it>
2022-10-10 18:23:26 +0200

    fix SimulationTest

--- src/test/scala/galaxy_sim/model/SimulationTest.scala
+++ src/test/scala/galaxy_sim/model/SimulationTest.scala
@@ -1,11 +1,11 @@
 package galaxy_sim.model
 
-import galaxy_sim.model.SimulationConfig.{blackHole, bounds, interstellarCloud}
+import galaxy_sim.model.SimulationConfig.{blackHole, bounds}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.must.Matchers.compile
 import org.scalatest.matchers.should.Matchers.should
 
 class SimulationTest  extends AnyFunSuite:
   test("Simulation correctly defines constructors"){
-    "Simulation(celestialBodies = Set(blackHole, interstellarCloud), bounds = bounds)" should compile
+    "Simulation(celestialBodies = Set(blackHole), bounds = bounds)" should compile
   }
