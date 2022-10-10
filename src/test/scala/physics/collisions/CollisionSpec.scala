1225c05b96f33f37e2177f902898575bd85f8e88
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-10-10 11:03:42 +0200

    created a single test specification for collisions

--- src/test/scala/physics/collisions/CollisionSpec.scala
+++ src/test/scala/physics/collisions/CollisionSpec.scala
@@ -6,8 +6,9 @@ import physics.Pair
 import physics.collisions.Collider.*
 import physics.collisions.CollisionDetection.CollisionBoxes.CircleCollisionBox
 import physics.collisions.CollisionDetection.CollisionCheckers.given
+import physics.collisions.CollisionMockups.{Nebula, Planet, Star, System}
 
-class CollisionDetectionSpec extends AnyFeatureSpec with GivenWhenThen:
+class CollisionSpec extends AnyFeatureSpec with GivenWhenThen:
 
   Feature("The user can see that two entities are colliding") {
     info("As a programmer")
@@ -32,5 +33,25 @@ class CollisionDetectionSpec extends AnyFeatureSpec with GivenWhenThen:
       Then("I'm able to check that it is not in place")
       assert(collider == Collider.None())
     }
+  }
 
+  Feature("The user can see the effect of a collision between entities") {
+    info("As a programmer")
+    info("I want to establish a mapping between colliding entities and the result of the collision")
+
+    Scenario("Two entities are colliding") {
+      Given("Two colliding entities")
+      val star = Star(Pair(0, 0), 2, 100)
+      val nebula = Nebula(Pair(1, 2), 10, 1)
+      val collidingStar = Collider(star)
+      When("I solve the collision")
+      val result = collidingStar >< nebula
+      Then("I can see the result of the collision")
+      val expectedResult = Some(System(
+        Pair(0.0, 0.0),
+        Star(Pair(0.0, 0.0), 2.0, 50.0),
+        List(Planet(Pair(1.0, 1.0), 0.1, 10.0), Planet(Pair(0.5, 1.5), 0.01, 1.0))
+      ))
+      assert(result equals expectedResult)
+    }
   }
