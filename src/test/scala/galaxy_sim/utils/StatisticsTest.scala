d5496ba24f55e8f3eab61c14773c8e08fed2e56e
Eddie Barzi <eddie.barzi@studio.unibo.it>
2022-10-17 13:21:03 +0200

    fix StatisticsTest

--- src/test/scala/galaxy_sim/utils/StatisticsTest.scala
+++ src/test/scala/galaxy_sim/utils/StatisticsTest.scala
@@ -1,4 +1,4 @@
-/* package galaxy_sim.utils
+package galaxy_sim.utils
 
 import galaxy_sim.model.SimulationConfig.{sun, earth, bounds, moon, blackHole}
 import galaxy_sim.model.{CelestialBodyType, Simulation, CelestialBody}
@@ -15,24 +15,23 @@ class StatisticsTest extends AnyFlatSpec with should.Matchers:
   )
 
   "planet quantity" should "be 2 (moon and earth)" in {
-    val simulation = Simulation(celestialBodies = celestialBodiesMap, bounds = bounds, virtualTime = 0, deltaTime = 1000)
-    val planetQuantity = Statistics.quantityOfThisCelestialBody(CelestialBodyType.Planet, simulation.celestialBodies)
+    val simulation = Simulation(galaxy = celestialBodiesMap, bounds = bounds, virtualTime = 0, deltaTime = 1000)
+    val planetQuantity = Statistics.quantityOfThisCelestialBody(CelestialBodyType.Planet, simulation.galaxy)
     planetQuantity shouldBe 2
   }
 
   "the simulation" should "contains 2 planets and 1 MassiveStar" in {
-    val simulation = Simulation(celestialBodies = celestialBodiesMap, bounds = bounds, virtualTime = 0, deltaTime = 1000)
-    val celestialBodiesQuantity = Statistics.numberOfCelestialBodiesForEachType(simulation.celestialBodies)
+    val simulation = Simulation(galaxy = celestialBodiesMap, bounds = bounds, virtualTime = 0, deltaTime = 1000)
+    val celestialBodiesQuantity = Statistics.numberOfCelestialBodiesForEachType(simulation.galaxy)
     //celestialBodiesQuantity.foreach((k, v) => println(s"tipo $k, Quantità $v"))
     celestialBodiesQuantity(CelestialBodyType.Planet) shouldBe 2
     celestialBodiesQuantity(CelestialBodyType.MassiveStar) shouldBe 1
   }
 
   "the simulation" should "contains 67% planet and 33% MassiveStar" in {
-    val simulation = Simulation(celestialBodies = celestialBodiesMap, bounds = bounds, virtualTime = 0, deltaTime = 1000)
-    val celestialBodiesPercentage = Statistics.percentageOfCelestialBodiesForEachType(simulation.celestialBodies)
+    val simulation = Simulation(galaxy = celestialBodiesMap, bounds = bounds, virtualTime = 0, deltaTime = 1000)
+    val celestialBodiesPercentage = Statistics.percentageOfCelestialBodiesForEachType(simulation.galaxy)
     //celestialBodiesPercentage.foreach((k, v) => println(s"tipo $k, Quantità $v"))
     celestialBodiesPercentage(CelestialBodyType.Planet) shouldBe 67
     celestialBodiesPercentage(CelestialBodyType.MassiveStar) shouldBe 33
   }
- */
