14e2cea12003906b9388a2ed95b9620927428f2e
Eddie Barzi <eddie.barzi@studio.unibo.it>
2022-10-18 14:19:31 +0200

    add entity check tests

--- src/test/scala/galaxy_sim/model/LifecycleTest.scala
+++ src/test/scala/galaxy_sim/model/LifecycleTest.scala
@@ -14,6 +14,30 @@ class LifecycleTest extends AnyFlatSpec with should.Matchers:
       entityIdentifierProlog.checkEntityType(10, 1010) shouldBe CelestialBodyType.MassiveStar
     }
 
+  "entity with mass 10e11 and temperature 1010" should "be a 'redSuperGiant'" in {
+    entityIdentifierProlog.checkEntityType(10e11, 1010) shouldBe CelestialBodyType.RedSuperGiant
+  }
+
+  "entity with mass 10e21 and temperature 1010" should "be a 'supernova'" in {
+    entityIdentifierProlog.checkEntityType(10e21, 1010) shouldBe CelestialBodyType.Supernova
+  }
+
+  "entity with mass 10e41 and temperature 1010" should "be a 'blackHole'" in {
+    entityIdentifierProlog.checkEntityType(10e41, 1010) shouldBe CelestialBodyType.BlackHole
+  }
+
+  "entity with mass 10 and temperature 101" should "be a 'InterstellarCloud'" in {
+    entityIdentifierProlog.checkEntityType(10, 101) shouldBe CelestialBodyType.InterstellarCloud
+  }
+
+  "entity with mass 10 and temperature 10" should "be a 'planet'" in {
+    entityIdentifierProlog.checkEntityType(10, 10) shouldBe CelestialBodyType.Planet
+  }
+
+  "entity with mass 10 and temperature 0" should "be a 'asteroid'" in {
+    entityIdentifierProlog.checkEntityType(10, 0) shouldBe CelestialBodyType.Asteroid
+  }
+
   "'Massive Star'" should "become 'Red Supergiant'"
 
   "'Red Supergiant'" should "become 'Supernova'"
