a26c341fe041034d78866a3fc0273d68ba3de05b
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-10-20 00:19:59 +0200

    first draft of a collider monad to use collision API inside for comprehension

--- src/main/scala/physics/ref/Collider.scala
+++ src/main/scala/physics/ref/Collider.scala
@@ -1,3 +1,21 @@
 package physics.ref
 
-case class Collider[A, B](fun: (A, B) => Collision[A, B])
+case class Collider[A](a: A):
+  def impactWith[B](other: B)(using col: Collision[A, B]): Collider[A] =
+    Collider(col.impact(a, other))
+  def flatMap[B](f: A => Collider[B]): Collider[B] =
+    f(a)
+  def map[B](f: A => B): Collider[B] =
+    Collider(f(a))
+
+
+object TryCollider extends App:
+  import Collisions.given
+
+  val a = 1
+  val b = 1
+  val col = Collider(a)
+  val res = for
+    y <- col.impactWith(b)
+  yield y
+  println(res)
