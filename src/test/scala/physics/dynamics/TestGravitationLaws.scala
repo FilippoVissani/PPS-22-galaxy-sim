f3d74e65704a52e7c34cd1e05ac5ac414c9b7889
Angela Cortecchia <angela.cortecchia@hotmail.com>
2022-10-19 10:21:18 +0200

    refactoring

--- src/test/scala/physics/dynamics/TestGravitationLaws.scala
+++ src/test/scala/physics/dynamics/TestGravitationLaws.scala
@@ -5,7 +5,7 @@ import org.scalatest.featurespec.AnyFeatureSpec
 import org.scalatest.funsuite.AnyFunSuite
 import physics.*
 
-import scala.math.{Pi, pow, sqrt, tan, tanh}
+import scala.math.{pow, sqrt}
 
 case class PhysicalEntityImpl(override val mass: Mass = 1000,
                               override val position: Position = Pair(0, 0),
@@ -16,18 +16,20 @@ case class PhysicalEntityImpl(override val mass: Mass = 1000,
 class TestGravitationLaws extends AnyFeatureSpec with GivenWhenThen:
   import GravitationLaws.*
 
-  Feature("The user can see an entity that moves around another one"){
-    info("As a programmer")
-    info("I want to check that the entity moves based on specific calculations")
+  info("As a programmer")
+  info("Considering some entities in the space")
+  info("I want to move entities around others")
+
+  Feature("Gravitational law calculations"){
     val earth = PhysicalEntityImpl(5.972e24, Pair(astronomicUnit * 10167, 0), 29290, Pair(0, 29290), Pair(0, 0))
     val sun = PhysicalEntityImpl(2.0e30)
     val deltaTime = daySec * 1 //one day
-    var earthFinal = PhysicalEntityImpl()
+    var earthFinal = earth
 
-    Scenario("The entity moves after some time"){
-      Given("Two entities - the Earth and the Sun")
+    Scenario("The entity moves around a bigger one after some time"){
+      Given("Two entities at their initial position - the Earth and the Sun")
 
-      When("I want to see the movement of the earth after some time")
+      When("Pass some time")
 
       val shouldDistance = Pair(earth.position.x - sun.position.x, earth.position.y - sun.position.y)
       val sunEarthDistance = distanceBetweenTwoEntities(earth, sun)
@@ -53,16 +55,22 @@ class TestGravitationLaws extends AnyFeatureSpec with GivenWhenThen:
       earthFinal = earthFinal.copy(position = vectorChangeOfDisplacement(earthFinal, deltaTime))
       assert(earthFinal.position == newPos)
 
-      Then("I'm able to move the entity in its new position, changing speed and gravity force on it")
+      Then("I can see the entity in its new position, changing speed and gravity force on it")
       val shouldEarth = PhysicalEntityImpl(5.972e24, newPos, 29290, shouldSpeedVector, shouldGForce)
       assert(earthFinal === shouldEarth)
+    }
 
-      Then("I'm also able to eventually move the entity reference into another point")
-      var sunFinal = sun.copy(speedVector = entityReferenceSpeedVectorAfterTime(sun, Set(earthFinal), deltaTime))
-      sunFinal = sunFinal.copy(position = vectorChangeOfDisplacement(sunFinal, deltaTime))
+    Scenario("The bigger entity moves due to the influence of the others"){
+      Given("Two entities - the Sun and the Earth")
       var shouldSun = sun.copy()
       val entities = Set(earthFinal)
-      shouldSun = shouldSun.copy(speedVector = Pair(sun.speedVector.x + (- entities.iterator.map(e => e.gForceVector.x).sum * deltaTime / sun.mass), sun.speedVector.y + (- entities.iterator.map(e => e.gForceVector.y).sum * deltaTime / sun.mass)))
+
+      When("When pass some time")
+      var sunFinal = sun.copy(speedVector = entityReferenceSpeedVectorAfterTime(sun, Set(earthFinal), deltaTime))
+      sunFinal = sunFinal.copy(position = vectorChangeOfDisplacement(sunFinal, deltaTime))
+
+      Then("I can also see the entity move reference into another point")
+      shouldSun = shouldSun.copy(speedVector = Pair(sun.speedVector.x + (-entities.iterator.map(e => e.gForceVector.x).sum * deltaTime / sun.mass), sun.speedVector.y + (-entities.iterator.map(e => e.gForceVector.y).sum * deltaTime / sun.mass)))
       shouldSun = shouldSun.copy(position = Pair(sun.position.x + (shouldSun.speedVector.x * deltaTime), sun.position.y + (shouldSun.speedVector.y * deltaTime)))
       assert(sunFinal == shouldSun)
     }
