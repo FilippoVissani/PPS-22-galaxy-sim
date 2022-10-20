f0a9e281c498fa667d3c8a8a8e70d1d4b6cfc72b
Eddie Barzi <eddie.barzi@studio.unibo.it>
2022-10-20 09:45:04 +0200

    the pieChart is visible (with default dataset)

--- src/main/scala/galaxy_sim/view/StatisticsFrame.scala
+++ src/main/scala/galaxy_sim/view/StatisticsFrame.scala
@@ -39,54 +39,10 @@ object StatisticsFrame:
   end StatisticsFrameImpl
 
   private class MainFrame extends JFrame :
-    val mainPanel: JPanel = JPanel(BorderLayout())
-    this.getContentPane.add(mainPanel)
-
-  private class StatisticsPanel extends JPanel :
-    var simulation: Option[Simulation] = Option.empty
-
-    def display(simulation: Simulation): Unit =
-      this.simulation = Option(simulation)
-      repaint()
-
-    override def paint(g: Graphics): Unit =
-      if simulation.isDefined then
-        val g2: Graphics2D = g.asInstanceOf[Graphics2D]
-        g2.setRenderingHint(
-          RenderingHints.KEY_ANTIALIASING,
-          RenderingHints.VALUE_ANTIALIAS_ON
-        )
-        g2.setRenderingHint(
-          RenderingHints.KEY_RENDERING,
-          RenderingHints.VALUE_RENDER_QUALITY
-        )
-        simulation.get.galaxy.values.foreach(v =>
-          v.foreach(e =>
-            g2.drawString(e.name, scaleX(e.position.x), scaleY(e.position.y))
-          )
-        )
-
-    override def getPreferredSize: Dimension =
-      val d: Dimension = this.getParent.getSize()
-      var newSize: Int = if d.width > d.height then d.height else d.width
-      newSize = if newSize == 0 then 100 else newSize
-      Dimension(newSize, newSize)
-
-    private def scaleX(value: Double): Int =
-      val percent = value * 100 / simulation.get.bounds.rightBound
-      val sizeOnDisplay = percent * this.getWidth / 100
-      Math.round(sizeOnDisplay + this.getWidth / 2).toInt
-
-    private def scaleY(value: Double): Int =
-      val percent = value * 100 / simulation.get.bounds.bottomBound
-      val sizeOnDisplay = percent * this.getHeight / 100
-      Math.round(sizeOnDisplay + this.getHeight / 2).toInt
-
-    private def pieChart(): Unit =
+    def createPieChartPanel(): ChartPanel =
       val dataset = DefaultPieDataset[String]()
       dataset.setValue("IPhone 5s", 5.0)
       dataset.setValue("SamSung Grand", 20.0)
-
 
       val chart = ChartFactory.createPieChart(
         "Mobile Sales", // chart title
@@ -98,7 +54,31 @@ object StatisticsFrame:
       chart.setBackgroundPaint(new GradientPaint(new Point(0, 0),
         new Color(20, 20, 20), new Point(400, 200), Color.DARK_GRAY));
 
-      chart.getPlot
+      ChartPanel(chart)
+
+    val mainPanel = createPieChartPanel()
+
+    this.getContentPane.add(mainPanel)
+
+  private class StatisticsPanel extends JPanel :
+    var simulation: Option[Simulation] = Option.empty
+
+    def display(simulation: Simulation): Unit =
+      this.simulation = Option(simulation)
+      //repaint()
+
+    override def paint(g: Graphics): Unit =
+      if simulation.isDefined then ???
+
+
+    override def getPreferredSize: Dimension =
+      val d: Dimension = this.getParent.getSize()
+      var newSize: Int = if d.width > d.height then d.height else d.width
+      newSize = if newSize == 0 then 100 else newSize
+      Dimension(newSize, newSize)
+
+
+
 
 
   end StatisticsPanel
