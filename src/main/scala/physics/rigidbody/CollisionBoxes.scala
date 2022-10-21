97c5734df0b4fe85d5d014b4d6f798b66dfdc107
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-10-21 15:03:04 +0200

    added doc

--- src/main/scala/physics/rigidbody/CollisionBoxes.scala
+++ src/main/scala/physics/rigidbody/CollisionBoxes.scala
@@ -3,5 +3,9 @@ package physics.rigidbody
 import physics.Position
 
 object CollisionBoxes:
+  /** A basic shape to be used alongside [[Intersection]] for implementing collision checking abstracting from the real
+   * shape of the objects we want to check. */
   sealed trait CollisionBox
+  
+  /** A circular [[CollisionBox]] */
   case class CircleCollisionBox(origin: Position, radius: Double) extends CollisionBox
