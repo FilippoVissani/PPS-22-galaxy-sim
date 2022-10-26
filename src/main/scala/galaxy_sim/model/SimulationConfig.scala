8659101ea7154f460c37fa27e241baefcc499c99
Filippo Vissani <filippo.vissani@studio.unibo.it>
2022-10-26 11:22:41 +0200

    Revert "change some values"
    This reverts commit f4ff2ac5418eaf0cdfd547a6b30b2f67a875bf1c.

--- src/main/scala/galaxy_sim/model/SimulationConfig.scala
+++ src/main/scala/galaxy_sim/model/SimulationConfig.scala
@@ -1,46 +1,46 @@
 package galaxy_sim.model
 
 import physics.dynamics.GravitationLaws.*
-import physics.{Pair, SpeedVector}
 import physics.dynamics.PhysicalEntity
+import physics.{Pair, SpeedVector}
 
 object SimulationConfig:
   /** Time between two requests of the simulation state. */
   val frameRate = 33
-  //  val bounds: Boundary = Boundary(0, astronomicUnit * 7, 0, astronomicUnit * 7)
-  val bounds: Boundary = Boundary(0, lightYear, 0, lightYear)
+  val bounds: Boundary = Boundary(0, astronomicUnit * 3, 0, astronomicUnit * 3)
+//  val bounds: Boundary = Boundary(0, lightYear * 3, 0, lightYear * 3)
   val blackHoleDistance = astronomicUnit * 5
-  val deltaTime = daySec //40 //e-10
+  val deltaTime = daySec
   val radiusScale = 1e4
 
   val moon: CelestialBody =
     CelestialBody(mass = earthMass * 0.0123,
-      aphelionSpeed = 3683,
-      gForceVector = Pair(0,0),
-      speedVector = Pair(0, 3683),
-      position = Pair(blackHoleDistance + astronomicUnit * 1.0167 + 384400, 0),
-      name = "Moon",
-      radius = 1_737.5 * radiusScale,
-      temperature = 110)
-  //    position = Pair(blackHoleDistance + astronomicUnit * 1.0167 + 384400, 0),
+    aphelionSpeed = 3683,
+    gForceVector = Pair(0,0),
+    speedVector = Pair(0, 3683),
+    position = Pair(astronomicUnit * 1.0167 + 384400, 0),
+    name = "Moon",
+    radius = 1_737.5 * radiusScale,
+    temperature = 110)
+//    position = Pair(blackHoleDistance + astronomicUnit * 1.0167 + 384400, 0),
 
   val sun: CelestialBody =
     CelestialBody(mass = solarMass,
       aphelionSpeed = 0,
       gForceVector = Pair(0, 0),
       speedVector = Pair(0, 50000),
-      position = Pair(blackHoleDistance, 0),
+      position = Pair(astronomicUnit * 3 / 2, astronomicUnit * 3 / 2),
       name = "Sun",
       radius = 695_508 * radiusScale,
       temperature = 1100)
-  //      position = Pair(blackHoleDistance, 0),
+//      position = Pair(blackHoleDistance, 0),
 
   val earth: CelestialBody =
     CelestialBody(mass = earthMass,
       aphelionSpeed = 29290,
       gForceVector = Pair(0, 0),
       speedVector = Pair(1, 29290),
-      position = Pair(blackHoleDistance + astronomicUnit * 1.0167, 0),
+      position = Pair(sun.position.x + astronomicUnit * 1.0167, sun.position.y + 0),
       name = "Earth",
       radius = 695_508 * radiusScale,
       temperature = 150)
@@ -50,19 +50,19 @@ object SimulationConfig:
       aphelionSpeed = 29290,
       gForceVector = Pair(0, 0),
       speedVector = Pair(-1, 29290),
-      position = Pair(- blackHoleDistance -astronomicUnit * 1.0167, 0),
-      name = "Earth2",
+      position = Pair(sun.position.x - astronomicUnit * 1.0167, sun.position.y + 0),
+      name = "Earth",
       radius = 695_508 * radiusScale,
       temperature = 150)
-  //      position = Pair(blackHoleDistance + astronomicUnit * 1.0167, 0),
+//      position = Pair(blackHoleDistance + astronomicUnit * 1.0167, 0),
 
   val blackHole: CelestialBody =
-    CelestialBody(mass = solarMass * 10,
+    CelestialBody(mass = solarMass * 5,
       aphelionSpeed = 0,
       gForceVector = Pair(0, 0),
       speedVector = Pair(0, 0),
       position = Pair(0, 0),
-      name = "Black hole",
+      name = "Sagittarius A*",
       radius = 30,
       temperature = 10)
 
@@ -71,7 +71,7 @@ object SimulationConfig:
       aphelionSpeed = 29290,
       gForceVector = Pair(0, 0),
       speedVector = Pair(0, 29290),
-      position = Pair( - blackHoleDistance -(astronomicUnit/2), 0),
+      position = Pair(-(astronomicUnit/2), 0),
       name = "Interstellar Cloud2",
       radius = 10,
       temperature = 0)
