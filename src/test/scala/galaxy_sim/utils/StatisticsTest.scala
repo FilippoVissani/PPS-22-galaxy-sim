30545cc4ab66421d6a682581d395be1956b9d3f5
Eddie Barzi <eddie.barzi@studio.unibo.it>
2022-10-27 17:39:59 +0200

    add new Statistics function to count the number of the bodies in the simulation

--- src/test/scala/galaxy_sim/utils/StatisticsTest.scala
+++ src/test/scala/galaxy_sim/utils/StatisticsTest.scala
@@ -20,16 +20,23 @@ class StatisticsTest extends AnyFlatSpec with should.Matchers:
     planetQuantity shouldBe 2
   }
 
-  "the simulation" should "contains 2 planets and 1 MassiveStar" in {
+  "the simulation" should "contain 2 planets and 1 MassiveStar" in {
     val simulation = Simulation(galaxy = celestialBodiesMap, bounds = bounds, virtualTime = 0, deltaTime = 1000)
     val celestialBodiesQuantity = Statistics.numberOfCelestialBodiesForEachType(simulation.galaxy)
     celestialBodiesQuantity(CelestialBodyType.Planet) shouldBe 2
     celestialBodiesQuantity(CelestialBodyType.MassiveStar) shouldBe 1
   }
 
-  "the simulation" should "contains 67% planet and 33% MassiveStar" in {
+  "the simulation" should "contain 67% planet and 33% MassiveStar" in {
     val simulation = Simulation(galaxy = celestialBodiesMap, bounds = bounds, virtualTime = 0, deltaTime = 1000)
     val celestialBodiesPercentage = Statistics.percentageOfCelestialBodiesForEachType(simulation.galaxy)
     celestialBodiesPercentage(CelestialBodyType.Planet) shouldBe 67
     celestialBodiesPercentage(CelestialBodyType.MassiveStar) shouldBe 33
+  }
+
+  "the simulation" should "contain 3 bodies" in {
+    val simulation = Simulation(galaxy = celestialBodiesMap, bounds = bounds, virtualTime = 0, deltaTime = 1000)
+    val quantityOfTotalBodies = Statistics.quantityOfTotalBodies(simulation.galaxy)
+    quantityOfTotalBodies shouldBe 3
+
   }
