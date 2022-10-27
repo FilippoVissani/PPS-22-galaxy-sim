26f9299ddd8627b907c24376faad628d2bd625d3
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-10-27 10:08:33 +0200

    a

--- src/main/scala/physics/Utils.scala
+++ src/main/scala/physics/Utils.scala
@@ -15,10 +15,8 @@ case class Pair(x: Double, y: Double)
 object Pair:
   def sum(p1: Pair, p2: Pair): Pair =
     Pair(p1.x + p2.x, p1.y + p2.y)
-
   def distance(p1: Pair, p2: Pair): Double =
     Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2))
-
   def module(p: Pair): Double =
     Math.sqrt(Math.pow(p.x, 2) + Math.pow(p.y, 2))
 
