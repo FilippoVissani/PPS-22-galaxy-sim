d29d201af01768f38e4bb0cbf2648e870ff3cf15
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-10-27 09:48:35 +0200

    augmented the quantity of the mass absorbed by the bigger body

--- src/main/scala/galaxy_sim/utils/SimulationGivens.scala
+++ src/main/scala/galaxy_sim/utils/SimulationGivens.scala
@@ -5,16 +5,17 @@ import galaxy_sim.model.CelestialBodyType.{BlackHole, InterstellarCloud, Massive
 import physics.collisions.impact.Impact
 import physics.collisions.instances.IntersectionInstances.given
 import physics.collisions.intersection.Intersection
+import physics.dynamics.PhysicalEntity
 
 object SimulationGivens:
   private def absorb(bigger: CelestialBody, smaller: CelestialBody): CelestialBody =
-    bigger.copy(mass = bigger.mass + smaller.mass / 2, temperature = bigger.temperature * 1.5)
+    bigger.copy(mass = bigger.mass + smaller.mass * 0.8, temperature = bigger.temperature * 1.5)
 
   private def disintegrate(smaller: CelestialBody): CelestialBody =
-    smaller.copy(mass = smaller.mass / 2, temperature = smaller.temperature * 0.8)
+    smaller.copy(mass = smaller.mass * 0.2, temperature = smaller.temperature * 0.8)
 
   given CelestialBodyIntersection: Intersection[CelestialBody] =
     Intersection.from((a1, a2) => CircleToCircleCollision.intersects(a1.collisionBox, a2.collisionBox))
 
   given CelestialBodyImpact: Impact[CelestialBody] =
-    Impact.from((a1, a2) => if a1.mass >= a2.mass then absorb(a1, a2) else disintegrate(a1))
+    Impact.from((a1, a2) => if a1.mass > a2.mass then absorb(a1, a2) else disintegrate(a1))
