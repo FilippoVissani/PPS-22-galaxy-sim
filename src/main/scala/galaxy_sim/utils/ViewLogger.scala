057bfb994f18bb0997a4e1fb782a15c39852e9b8
Angela Cortecchia <angela.cortecchia@hotmail.com>
2022-10-28 16:34:30 +0200

    add doc

--- src/main/scala/galaxy_sim/utils/ViewLogger.scala
+++ src/main/scala/galaxy_sim/utils/ViewLogger.scala
@@ -27,7 +27,6 @@ trait ViewLogger:
    */
   def newBody(celestialBody: CelestialBody): Unit
 
-  //todo add rest of doc
   /**
    *
    * @param bodiesInvolved
@@ -35,35 +34,59 @@ trait ViewLogger:
    * @return
    */
   def bodiesLogger(bodiesInvolved: (CelestialBody, Option[CelestialBody]), description: LoggerActions): String
+
+  /**
+   * Updates the info of a certain body
+   * @param celestialBody the body to update
+   */
   def bodyUpdated(celestialBody: CelestialBody): Unit
+
+  /**
+   * Update the galaxy when a body dies
+   * @param celestialBody the body that died
+   */
   def bodyDied(celestialBody: CelestialBody): Unit
+
+  /**
+   * Gets the names of all the bodies
+   * @return a list containing all bodies' names
+   */
   def getBodiesNames: List[String]
-  def bodyInfos(bodyName: String): Option[CelestialBody]
+
+  /**
+   * Get the info of a specific body
+   * @param bodyName the name of the body
+   * @return
+   */
+  def bodyInfos(bodyName: String): String
 
 object ViewLogger:
-  def apply(): ViewLogger =
-    ViewLoggerImpl()
+  def apply(): ViewLogger = ViewLoggerImpl()
 
   private class ViewLoggerImpl() extends ViewLogger:
     var galaxyList: List[CelestialBody] = List.empty
 
-    override def simulationGalaxy(galaxy: Map[CelestialBodyType, Set[CelestialBody]]): Unit = galaxyList = galaxy.values.filter(x => x.nonEmpty).flatten.toList //todo synchronized ?
+    override def simulationGalaxy(galaxy: Map[CelestialBodyType, Set[CelestialBody]]): Unit = synchronized{ galaxyList = galaxy.values.filter(x => x.nonEmpty).flatten.toList }
 
-    override def bodyDied(celestialBody: CelestialBody): Unit = galaxyList = galaxyList.filterNot(b => b.name == celestialBody.name) //todo synchronized ?
+    override def bodyDied(celestialBody: CelestialBody): Unit = galaxyList = synchronized{ galaxyList.filterNot(b => b.name == celestialBody.name) }
 
-    override def bodyUpdated(celestialBody: CelestialBody): Unit = galaxyList = galaxyList.filterNot(b => b.name == celestialBody.name).::(celestialBody) //todo synchronized ?
+    override def bodyUpdated(celestialBody: CelestialBody): Unit = galaxyList = synchronized{ galaxyList.filterNot(b => b.name == celestialBody.name).::(celestialBody) }
 
-    override def newBody(celestialBody: CelestialBody): Unit = galaxyList = galaxyList.::(celestialBody) //todo synchronized ?
+    override def newBody(celestialBody: CelestialBody): Unit = synchronized { galaxyList = galaxyList.::(celestialBody) }
 
     override def getBodiesNames: List[String] = galaxyList.map(b => b.name)
 
-    override def bodyInfos(bodyName: String): Option[CelestialBody] = galaxyList.find(b => b.name == bodyName)
+    override def bodyInfos(bodyName: String): String =
+      val body = galaxyList.find(b => b.name == bodyName)
+      val res = ""
+      if body.isDefined then
+        res + s"Name: ${body.get.name.toUpperCase}\n" +
+          s"Position: (${body.get.position.x.toString.substring(0, 4)} , ${body.get.position.y.toString.substring(0, 4)})\n" +
+          s"Speed: ${body.get.speedVector.y/1000} Km/s\n" +
+          s"Mass = ${body.get.mass} kg\n" +
+          s"Temperature = ${body.get.temperature.toString.substring(0,4)} °C\n\n"
+      else res + "Not found"
 
     override def bodiesLogger(bodiesInvolved: (CelestialBody, Option[CelestialBody]), description: LoggerActions): String =
-      val res = s"${bodiesInvolved._1.name} just ${description.toString.toUpperCase}\n"
-      if bodiesInvolved._2.isDefined then res + s" with ${bodiesInvolved._2.get.name}"
-      res
-      /*description match
-      case Collided => s"${body.name} ${Collided.toString.toUpperCase}\n"
-      case Spawn => s"${bodiesInvolved._1.name.toUpperCase} just ${Spawn.toString.toUpperCase}\n"
-      case Died => s"${bodiesInvolved._1.name.toUpperCase} just ${Died.toString.toUpperCase}\n"*/
+      val res = s"${bodiesInvolved._1.name} just ${description.toString.toLowerCase}"
+      if bodiesInvolved._2.isDefined then res + s" with ${bodiesInvolved._2.get.name}" else res
