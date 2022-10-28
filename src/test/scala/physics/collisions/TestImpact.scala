408ef79e2b5809dd22b2e9314098c1ef4c54eb45
LEONARDO MICELLI <leonardomicelli@gmail.com>
2022-10-28 18:12:31 +0200

    added a test case

--- src/test/scala/physics/collisions/TestImpact.scala
+++ src/test/scala/physics/collisions/TestImpact.scala
@@ -13,3 +13,10 @@ class TestImpact extends AnyFlatSpec:
     val res = star |*| star1
     assert(res == Star(Pair(0.0,0.0),2.0,100.66666666666667))
   }
+
+  it should "not produce a different value from the first argument if the two aren't colliding" in {
+    val star = Star(Pair(0, 0), 2, 100)
+    val star1 = Star(Pair(10, 20), 10, 1)
+    val res = star |*| star1
+    assert(res == star)
+  }
