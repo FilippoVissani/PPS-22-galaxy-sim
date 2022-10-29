1aac67d6167453d3dc9a7376b33ab1d8b3cf3f02
Angela Cortecchia <angela.cortecchia@hotmail.com>
2022-10-29 23:25:03 +0200

    add logger actor

--- src/main/scala/galaxy_sim/Main.scala
+++ src/main/scala/galaxy_sim/Main.scala
@@ -1,15 +1,17 @@
 package galaxy_sim
 
-import akka.actor.{ActorRef, scala2ActorRef}
+import akka.actor.ActorRef
 import akka.actor.typed.{ActorSystem, Behavior, Terminated}
 import akka.actor.typed.scaladsl.Behaviors
 import galaxy_sim.actors.ControllerActor.*
+import galaxy_sim.actors.LoggerActor.*
 import galaxy_sim.actors.ViewActor.LoggerMessage
-import galaxy_sim.actors.{CelestialBodyActor, ControllerActor, SimulationManagerActor, ViewActor}
+import galaxy_sim.actors.{CelestialBodyActor, ControllerActor, LoggerActor, SimulationManagerActor, ViewActor}
 import galaxy_sim.model.CelestialBodyType.*
 import galaxy_sim.model.SimulationConfig.*
 import galaxy_sim.model.{CelestialBody, CelestialBodyType, Simulation, emptyGalaxy}
-import galaxy_sim.utils.LoggerActions.*
+
+import scala.language.postfixOps
 
 object Main extends App:
   ActorSystem(RootActor(), "root")
@@ -27,15 +29,18 @@ MassiveStar -> Set(body01),
         InterstellarCloud -> groupOFInterstellarClouds(100)
       )
       //todo spawn logger actor
+      val loggerActor = ctx.spawn(LoggerActor(), "logger")
       val celestialBodyActors = galaxy
-      .map((k, v) => (k, v.map(x => ctx.spawnAnonymous(CelestialBodyActor(x, k, bounds, deltaTime))))) //todo pass logger actor
+      .map((k, v) => (k, v.map(x => ctx.spawnAnonymous(CelestialBodyActor(x, k, bounds, deltaTime, loggerActor))))) //todo pass logger actor
       .values
       .flatten
       .toSet
+      celestialBodyActors.foreach( x => loggerActor ! SpawnedBody(x))
       val simulationManagerActor = ctx.spawn(SimulationManagerActor(celestialBodyActors, Simulation(galaxy = galaxy, bounds, 0, deltaTime)), "simulationManager")
       val controllerActor = ctx.spawn(ControllerActor(Option.empty, simulationManagerActor), "controller")
-      val viewActor = ctx.spawn(ViewActor(controllerActor), "view") //todo pass logger actor
+      val viewActor = ctx.spawn(ViewActor(controllerActor, loggerActor), "view") //todo pass logger actor
       controllerActor ! SetView(viewActor, galaxy)
+      loggerActor ! SetViewActor(viewActor)
       Behaviors.receive[RootActorCommand]((ctx, msg) => msg match
         case _ => {
           Behaviors.stopped
